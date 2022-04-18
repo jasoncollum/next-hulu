fb83ad2937a52a601a0d239b6950d8a70572f0f6
Jason Collum <jasoncollum@gmail.com>
2022-04-18 09:42:22 -0500

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  mode: 'jit',
+  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
+  darkMod: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      screens: {
+        "3xl": "2000px",
+      }
+    },
+  },
+  plugins: [require('tailwind-scrollbar-hide')],
+}
